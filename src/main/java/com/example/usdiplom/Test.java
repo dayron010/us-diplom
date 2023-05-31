23fa74dc25afb679e6760f9ed6d51b6ba5dbd617
Diyorbek010 <dayerjabborov@gmail.com>
2023-05-31 12:26:08 +0500

    add test class

---
+++ src/main/java/com/example/usdiplom/Test.java
@@ -0,0 +1,11 @@
+package com.example.usdiplom;
+
+public class Test {
+
+    public static void main(String[] args) {
+        
+        System.out.println("nimadir");
+        System.out.println("\"piq\" = " + "piq");
+        
+    }
+}
